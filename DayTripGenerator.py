88eb31779685f333a685286107a51ef8c3cfbf55
Rodney McGee <rodneymcgee73@gmail.com>
2022-05-12 14:58:57 -0500

    Created Four Functions For DayTripGenerator.py

--- DayTripGenerator.py
+++ DayTripGenerator.py
@@ -5,14 +5,21 @@ form_of_entertainment = ["Concert", "Hiking", "Water Park", "Sightseeing"]
 
 
 import random
-random_destination = (random.choice(destination_choices))
-print(random_destination)
+def give_destination():
+    random_destination = (random.choice(destination_choices))
+    print(random_destination)
 
-random_resturant = (random.choice(resturant_choices))
-print(random_resturant)
+def give_resturant():
+    random_resturant = (random.choice(resturant_choices))
+    print(random_resturant)
 
-random_mode_of_transportation = (random.choice(mode_of_transportation))
-print(random_mode_of_transportation)
+def give_transportation():
+    random_mode_of_transportation = (random.choice(mode_of_transportation))
+    print(random_mode_of_transportation)
 
-random_form_of_entertainment = (random.choice(form_of_entertainment))
-print(random_form_of_entertainment)
+def give_entertainment():
+    random_form_of_entertainment = (random.choice(form_of_entertainment))
+    print(random_form_of_entertainment)
+
+
+print(give_destination)
