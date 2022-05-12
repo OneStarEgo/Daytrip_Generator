679ba84ac3e14da95e624da3e364b55f8e1879d3
Rodney McGee <rodneymcgee73@gmail.com>
2022-05-12 16:41:31 -0500

    Removed parenthesis

--- DayTripGenerator.py
+++ DayTripGenerator.py
@@ -6,19 +6,19 @@ form_of_entertainment = ["Concert", "Hiking", "Water Park", "Sightseeing"]
 
 import random
 def give_destination():
-    random_destination = (random.choice(destination_choices))
+    random_destination = random.choice(destination_choices)
     print(random_destination)
 
 def give_resturant():
-    random_resturant = (random.choice(resturant_choices))
+    random_resturant = random.choice(resturant_choices)
     print(random_resturant)
 
 def give_transportation():
-    random_mode_of_transportation = (random.choice(mode_of_transportation))
+    random_mode_of_transportation = random.choice(mode_of_transportation)
     print(random_mode_of_transportation)
 
 def give_entertainment():
-    random_form_of_entertainment = (random.choice(form_of_entertainment))
+    random_form_of_entertainment = random.choice(form_of_entertainment)
     print(random_form_of_entertainment)
 
 
